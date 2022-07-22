6f598c938dbdb7c7985a1f39ebd2befd1a8d1f9a
Pelin Mutanguha <pelmut2000@gmail.com>
2022-07-22 15:44:54 +0200

    Add speaker model

--- conference/admin.py
+++ conference/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from conference.models import Speaker
 
-# Register your models here.
+admin.site.register(Speaker)
--- conference/models.py
+++ conference/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    profile_picture = models.ImageField(upload_to='profiles/')
+
+    def __str__(self):
+        return self.name
