5908154f07f73500146207cb6af0d2c57b011e96
Pelin Mutanguha <pelmut2000@gmail.com>
2022-07-23 10:02:22 +0200

    add static urls to server images in admin

--- conference_django/urls.py
+++ conference_django/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,5 +12,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
 
     # conference urls
-     path('conference/', include('conference.urls'))
-]
+    path('conference/', include('conference.urls'))
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
